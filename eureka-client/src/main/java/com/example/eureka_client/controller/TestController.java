debfe362a2e51d29e6681c43f799c9eb97b87a25
Andrey <4057163@mail.ru>
2026-01-12 05:32:12 +0300

    4 commit

--- eureka-client/src/main/java/com/example/eureka_client/controller/TestController.java
+++ eureka-client/src/main/java/com/example/eureka_client/controller/TestController.java
@@ -1,13 +1,18 @@
 package com.example.eureka_client.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
-    @GetMapping("/test")
+    @Value("${eureka.instance.instance-id}")
+    private String value;
 
+    @GetMapping("/test")
     public String test(){
-        return "test";
+        return value;
     }
 }
